cd4366cce3df4cc45dbf4b5bb8b775454364fa13
andrew.sales <andjsales@gmail.com>
2025-06-28 15:55:31 -0500

    set up PreparedStatement for querying tv shows

--- src/main/java/com/example/progress/dao/impl/TVShowDaoImpl.java
+++ src/main/java/com/example/progress/dao/impl/TVShowDaoImpl.java
@@ -2,16 +2,55 @@
 
 package com.example.progress.dao.impl;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.example.progress.connection.ConnectionManager;
 import com.example.progress.dao.TVShowDao;
 import com.example.progress.model.TVShow;
 // import java.sql.SQLException;
+import com.example.progress.model.User;
 
 
 public class TVShowDaoImpl implements TVShowDao {
     // @SuppressWarnings("unused")
     @Override
     public TVShow findTVShowByTitle(int id, String title, int totalEpisodes) {
-        String sql = "SELECT * FROM TVShow WHERE id = ? AND title = ? AND totalEpisodes";
+        String sql = "SELECT * FROM TVShow WHERE id = ? AND title = ? AND totalEpisodes = ?";
+
+        try (Connection conn = ConnectionManager.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.setString(2, title);
+            stmt.setInt(3, totalEpisodes);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int dbId = rs.getInt("id");
+                String dbTitle = rs.getString("title");
+                int dbTotalEpisodes = rs.getInt(totalEpisodes);
+
+                return new TVShow(dbId, dbTitle, dbTotalEpisodes);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error finding user by credentials.");
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            System.out.println("Database configuration file not found.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("IO Exception occurred while getting connection.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver class not found.");
+            e.printStackTrace();
+        }
         return null;
+
     }
 }
