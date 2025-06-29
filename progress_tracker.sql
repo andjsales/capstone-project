da77ab778d852e17489a19421809f8338fd69d57
andrew.sales <andjsales@gmail.com>
2025-06-29 00:20:05 -0500

    drop tables and add sample data

--- progress_tracker.sql
+++ progress_tracker.sql
@@ -3,6 +3,10 @@ CREATE DATABASE IF NOT EXISTS progress_tracker;
 USE progress_tracker;
 
 
+DROP TABLE IF EXISTS UserTVShowTracker;
+DROP TABLE IF EXISTS TVShow;
+DROP TABLE IF EXISTS User;
+
 -- each user has a unique——ID + username + password
 CREATE TABLE User (
     id INT AUTO_INCREMENT PRIMARY KEY, -- AUTO_INCREMENT——ID increases automatically
@@ -27,9 +31,12 @@ CREATE TABLE UserTVShowTracker (
     user_id INT,
     tv_show_id INT,
     progress INT DEFAULT 0,
-    status ENUM('Plan to Watch', 'Watching', 'Completed') NOT NULL, --ENUM——restricts status values
-    rating INT CHECK (rating BETWEEN 1 AND 10), -- CHECK——ensures ratings are between 1-10
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE, -- ON DELETE CASCADE——deleting a user or show removes related tracker rows
+    -- ENUM restricts status values
+    status ENUM('Plan to Watch', 'Watching', 'Completed') NOT NULL,
+    -- CHECK ensures ratings are between 1-10
+    rating INT CHECK (rating BETWEEN 1 AND 10),
+    -- ON DELETE CASCADE: deleting a user or show removes related tracker rows
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
     FOREIGN KEY (tv_show_id) REFERENCES TVShow(id) ON DELETE CASCADE
 );
 
@@ -47,5 +54,5 @@ INSERT INTO UserTVShowTracker (user_id, tv_show_id, progress, status, rating) VA
 (1, 2, 244, 'Completed', 10),
 (1, 3, 51, 'Completed', 7),
 (2, 1, 51, 'Watching', NULL),
-(2, 2, 244, 'Completed', 10),
+(2, 2, 2, 'Watching', 10),
 (2, 3, 0, 'Plan to Watch', NULL);
